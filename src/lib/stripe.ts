82efc6491e84c38caacaee75520bfe12ba97e102
Matheus <matheus.dafonseca2004@gmail.com>
2023-02-09 19:30:59 -0300

    second commit

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,11 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2022-11-15',
-  appInfo: {
-    name: 'Ignite Shop',
+export const stripe = new Stripe(
+  'sk_test_51MX6JwG6g89uo2s3gjFezoKkeTbEvfKthco7kjLLdYMWyklKtOu8K5Z4rikmpxQfziGtQrejcQVpMBxw7HwLA4gW00370VV9dh',
+  {
+    apiVersion: '2022-11-15',
+    appInfo: {
+      name: 'Ignite Shop',
+    },
   },
-})
+)
